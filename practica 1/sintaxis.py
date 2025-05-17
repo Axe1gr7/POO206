1d3fe1b2c2183282d4265d653d65afddd6f5f301
Axe1gr7 <122043202@upq.edu.mx>
2025-05-16 18:37:41 -0600

    parte final de la practica

--- practica 1/sintaxis.py
+++ practica 1/sintaxis.py
@@ -9,4 +9,41 @@ print('yo soy la otra')
 
 variable1="hola soy una cadena"
 
-print(len(variable1))
+print(len(variable1)) 
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3- variable
+
+x="axelgr"
+x=4
+x=7
+print(x)
+
+y= int(3)
+print(x,y)
+print(y)
+print(type(x),type(y))
+
+#4.- solucionde datos
+
+a=input("introduce un numero")
+b=int(input("introduce un numero entero "))
+c=float(input("introduce un numero flotante "))
+print(a,b,c)
+
+#5.- boolean, comparacion y logicos
+
+print(10>9)
+print(10<9)
+print(10==9)
+print(10>=9)
+print(10<=9)
+print(10!=9) 
+
+x=1
+print(x<5 and x<10)
+print(x<5 or x<10)
+print(not(x<5 and x<10))
+
