24fa3c1e72cadf60d774d0220b8920c4ab0c14d9
Axe1gr7 <122043202@upq.edu.mx>
2025-05-28 14:49:53 -0600

    repeticion de letras

---
+++ practica 4/letras en la frase.py
@@ -0,0 +1,22 @@
+print("\nprograma que hace una cuenta regresiva del numero que ingresa el usuario al 0\n")
+r=(int)
+w=0
+while w==0:
+    r = int(input("quieres volver a intentar oprime 0 : "))
+    if r == 0:
+        try:
+           frase = input("\ningresa una frase : ")
+           if any(char.isdigit() for char in frase):
+               raise ValueError("valor no reconocido ingresa solo letras. vuelva a intentar")
+           else:
+            letra = input("Escribe una letra: ")
+            if len(letra) == 1:
+                cantidad = frase.count(letra)
+                print(f"La letra '{letra}' aparece {cantidad} veces.")
+            else:
+                print("Debes ingresar solo una letra.")
+        except ValueError as error:
+            print("error:",error) 
+    else:
+        print("vuelva pronto")
+        break
