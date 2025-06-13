73c4432b24232251d88711b975f78aa8b5fd3ff2
Axe1gr7 <122043202@upq.edu.mx>
2025-06-12 20:08:49 -0600

    practica 7

--- flask_proyect/app.py
+++ flask_proyect/app.py
@@ -2,10 +2,32 @@
 from flask import Flask 
 
 app= Flask (__name__)
-
+#ruta simple 
 @app.route('/')
 def home():
     return 'Hola mundo FLASK'
+
+#ruta con parametros 
+@app.route('/saludo/<nombre>')
+def saludar(nombre):
+    return 'Hola,'+nombre+'¡¡¡'
+
+#RUTA TRY-CATCH PARA ERRORES
+@app.errorhandler(404)
+def PagNoE(e): 
+    return 'CUIDADO: ERROR DE CAPA 8 ¡¡¡'
+
+#ruta doble 
+@app.route('/usuario')
+@app.route('/usuaria')
+def dobleroute():
+    return 'mismo servidor'
+
+#ruta post
+@app.route('/formulario' ,methods=['POST'])
+def formulario():
+    return 'soy un formulario'
+
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
