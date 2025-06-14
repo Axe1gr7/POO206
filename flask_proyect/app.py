4c5af3810f89751b5f0ba6053cdd34dc530c4a42
Axe1gr7 <122043202@upq.edu.mx>
2025-06-13 18:57:40 -0600

    practica 7 conexion bd

--- flask_proyect/app.py
+++ flask_proyect/app.py
@@ -1,7 +1,27 @@
+from flask import Flask,jsonify
+from flask_mysqldb import MySQL
+import MySQLdb
 
-from flask import Flask 
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = "1310041310004"
+app.config['MYSQL_DB'] = "DB_flask"
 
-app= Flask (__name__)
+mysql = MySQL(app)
+
+#ruta para checar la conexion a la bd 
+@app.route('/db_check')
+def db_check():
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.excute('select 1')
+        return jsonify({'status':'ok','message':'conexion exitosa'}),200        
+    except MySQLdb.MySQLError as e: jsonify({'status':'error','message':str(e)}),500
+     
+ 
+
+
 #ruta simple 
 @app.route('/')
 def home():
